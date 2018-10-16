2b9afe78b526a3ea03d6680f6d0c1f8fee8d7cea
cai xingliang <xingliang_2000@yahoo.com>
2018-10-17 07:46:30 +0800

    Add start script

--- route.js
+++ route.js
@@ -1,19 +1,29 @@
 var express=require('express')
 
 var router=express.Router()
+var headers={
+"Access-Control-Allow-origin": "*",
+"Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+"Access-Control-Allow-Headers": "Authorization, Content-Type",
+"Access-Control-Expose-Headers": "Authorization"
+}
+
+
 
 router.use(function timeLog(req,res,next){
     var dt=new Date();
     console.log(dt.toLocaleString() , "\t", req.ip , "\t", req.method + ' to: ' , req.originalUrl,  )
+    res.set(headers)
     next()
 })
 
-var headers={'Access-Control-Allow-Origin': '*',
-'Content-Type': 'Application/Json'
-}
+// var headers={
+//     'Access-Control-Allow-Origin': '*',
+//     'Content-Type': 'Application/Json'
+// }
 
 var channelList=["can", "gps", "sin", "asimov", "eth", "mobileeye"]
-router.get("/pandora/api/status", (req,res) =>{
+router.use("/pandora/api/status", (req,res) =>{
     var status={}
     channelList.forEach((item, index) =>  {
         var rad=Math.random()
@@ -32,6 +42,70 @@ router.get("/pandora/api/status", (req,res) =>{
     res.send(JSON.stringify(data))   
 
     
+})
+
+router.get("/fd", (req,res,next) =>{
+    console.log(req.path)
+    res.json(req.data)
+})
+
+
+router.post("/auth/login/", function(req,res) {
+    var token="MyaToken"
+    var tokenHeader="Bearer " + token
+    res.set({
+        "Authorization": tokenHeader
+    })
+    var data={
+        data: {
+            id: 1,
+            name: "Cai Xingliang",
+            roles: ["admin","user"]
+        }
+    }
+    res.json(data)
+})
+
+router.use("/auth/user/", function(req,res) {
+    var token="MyaToken"
+    var tokenHeader="Bearer " + token
+    res.set({
+        "Authorization": tokenHeader
+    })
+    var data={
+        data: {
+            id: 1,
+            name: "Cai Xingliang",
+            roles: ["admin","user"]
+        }
+    }
+    setTimeout(function(){res.json(data)},5000)
+})
+
+router.use("/auth/refresh/", function(req,res) {
+    var token="MyaToken"
+    var tokenHeader="Bearer " + token
+    res.set({
+        "Authorization": tokenHeader
+    })
+    var data={
+        data: {
+            id: 1,
+            name: "Cai Xingliang",
+            roles: ["admin","user"]
+        }
+    }
+    res.json(data)
+})
+
+
+
+router.use("/auth/logout/", function(req,res) {
+    
+    var data={
+        result_code: "success"
+    }
+    res.json(data)
 })
 
 module.exports=router
--- test/index.js
+++ test/index.js
@@ -1,1 +1,16 @@
 console.log("Test is skipped!")
+
+
+testit()
+
+
+function testit(){
+    logit()
+    console.log("After function return call")
+}
+
+
+function logit(){
+    console.log("Inside function")
+    return
+}
